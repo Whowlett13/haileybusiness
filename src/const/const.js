1fb366960804546a484613c82ef6ca2b908f0a50
Wyatt Howlett <wyatthowlett@yahoo.com>
2024-07-03 02:41:04 -0400

    commiting  tempate

--- src/const/const.js
+++ src/const/const.js
@@ -1,17 +1,17 @@
 export const studies = [
   {
-    title: "Técnico en desarrollo de software",
-    corporation: "SENA - Montería (Córdoba) ",
+    title: "Lorem ipsum dolor sit amet",
+    corporation: "Lorem ipsum dolor sit amet",
     date: "02/2019 – 12/2019",
   },
   {
-    title: "Desarrollador Front-end React Js",
-    corporation: "Alura Latam - Remoto",
+    title: "Lorem ipsum dolor sit amet",
+    corporation: "Lorem ipsum dolor sit amet",
     date: "04/2023 – 09/2023",
   },
   {
-    title: "Curso profesional de React Js",
-    corporation: "Código facilito - Remoto",
+    title: "Lorem ipsum dolor sit amet",
+    corporation: "Lorem ipsum dolor sit amet,",
     date: "04/2023 – 04/2023",
   },
 ];
@@ -132,8 +132,8 @@ export const projects = [
   {
     img: "../../../img/tasks-manager.webp",
     title: "Tasks Manager",
-    link: "https://task-manager-ecru-chi.vercel.app/dashboard",
-    description: `Administrador de tareas con drag and drop autenticación con Clerk , shadcn.  "En Desarrollo "`,
+    link: "https://www.wyatthowlett.ca",
+    description: `Lorem ipsum dolor sit amet`,
     technologies: [
       {
         icon: IconKeys.typeScript,
@@ -155,8 +155,8 @@ export const projects = [
   {
     img: "../../../img/ecommerce-stripe-2.webp",
     title: "Ecommerce Stripe",
-    link: "https://ecommerce-stripe-763m.vercel.app/",
-    description: `Proyecto FullStack de un ecommerce con pasarela de pagos stripe integrada, destalles de el product etc.  `,
+    link: "https://www.wyatthowlett.ca",
+    description: `"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."`,
     technologies: [
       {
         icon: IconKeys.typeScript,
@@ -177,9 +177,9 @@ export const projects = [
   },
   {
     img: "../../../img/nexanime.webp",
-    title: "NexAnime",
-    link: "https://nexanime.vercel.app",
-    description: `plataforma de anime para ver los animes en tendencia , más valorados , estadísticas.`,
+    title: "me",
+    link: "https://www.wyatthowlett.ca",
+    description: `Lorem ipsum dolor sit amet`,
     technologies: [
       {
         icon: IconKeys.typeScript,
@@ -195,10 +195,8 @@ export const projects = [
   {
     img: "../../../img/store-games.webp",
     title: "Store games",
-    link: "https://store-games-site.netlify.app/",
-    description: `Simulando una página de juegos , consumiendo la API de rawg api
-      donde los usuarios pueden buscar videojuegos, ver sus detalles como
-      creadores , calificaciones,tiendas donde comprar.....`,
+    link: "https://www.wyatthowlett.ca",
+    description: `Lorem ipsum dolor sit amet`,
     technologies: [
       {
         icon: IconKeys.react,
@@ -220,9 +218,8 @@ export const projects = [
   {
     img: "../../../img/movie.webp",
     title: "Movie Tv",
-    link: "https://movietvsite.netlify.app/",
-    description: `simulando una página de películas , consumiendo la API de themovietv
-              donde los usuarios pueden buscar películas y ver trailer.....`,
+    link: "https://www.wyatthowlett.ca",
+    description: `Lorem ipsum dolor sit amet`,
     technologies: [
       {
         icon: IconKeys.react,
@@ -241,10 +238,8 @@ export const projects = [
   {
     img: "../../../img/cyberpunk.webp",
     title: "cyberpunk",
-    link: "https://preeminent-liger-266356.netlify.app/",
-    description: `este proyecto se trata de un clon de la pagína de cyberpunk , donde
-      el ususario puede ver los tailer de él juegos y también los
-      episodios de la serie....`,
+    link: "https://www.wyatthowlett.ca",
+    description: `Lorem ipsum dolor sit amet`,
     technologies: [
       {
         icon: IconKeys.html,
@@ -261,10 +256,8 @@ export const projects = [
   {
     img: "../../../img/fit-nation.webp",
     title: "gym",
-    link: "https://fitnation.netlify.app/",
-    description: `landing page de una gym por ahora es una landing todavia está en
-              desarrollo la idea es que los usuarios puedan comprar productos de
-              el gym`,
+    link: "https://www.wyatthowlett.ca",
+    description: `Lorem ipsum dolor sit amet`,
     technologies: [
       {
         icon: IconKeys.react,
@@ -283,8 +276,8 @@ export const projects = [
   {
     img: "../../../img/spotify.webp",
     title: "Spotify-Clone",
-    link: "https://www.linkedin.com/posts/osnaider-mart%C3%ADnez_clon-de-la-p%C3%A1gina-de-spotify-echo-con-react-activity-7119712602342965248-upgL?utm_source=share&utm_medium=member_desktop",
-    description: `Clon de la página de Spotify con autenticación de usuario, reproductor de canciones, etc `,
+    link: "https://www.wyatthowlett.ca",
+    description: `Lorem ipsum dolor sit amet`,
     technologies: [
       {
         icon: IconKeys.react,
@@ -307,9 +300,8 @@ export const projects = [
   {
     img: "../../../img/landing.webp",
     title: "Landing Realidad Virtual",
-    link: "https://realityvirtual.netlify.app/",
-    description: `landing page de gafas de realidad virtual donde se muestra el
-              producto y como funciona`,
+    link: "https://www.wyatthowlett.ca",
+    description: `Lorem ipsum dolor sit amet`,
     technologies: [
       {
         icon: IconKeys.astro,
